a036b6db5bc547ffdca852b007d943eab22c3ff9
Nikhil Musale <nikhilmusale70@gmail.com>
2021-09-27 20:26:22 +0530

    [Nikhil] -  added initial setup

---
+++ src/test/java/com/payroll/employee_payroll/EmployeePayrollAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payroll.employee_payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
