d93fff8955e5146097ac8e98e56e6398931fff33
Nikhil Musale <nikhilmusale70@gmail.com>
2021-09-29 13:05:22 +0530

    [Nikhil] - added logging

--- src/main/java/com/payroll/employee_payroll/EmployeePayrollAppApplication.java
+++ src/main/java/com/payroll/employee_payroll/EmployeePayrollAppApplication.java
@@ -2,7 +2,7 @@ package com.payroll.employee_payroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.ApplicationContext;
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
@@ -12,8 +12,10 @@ public class EmployeePayrollAppApplication {
 
 	
 	public static void main(String[] args) {
-		log.error("Application is working, and logging is working fine");
-		SpringApplication.run(EmployeePayrollAppApplication.class, args);
+		ApplicationContext context = SpringApplication.run(EmployeePayrollAppApplication.class, args);
+		
+		log.info("Employee Payroll App started in {} Envoronment",
+				context.getEnvironment().getProperty("environment"));
 	}
 
 }
